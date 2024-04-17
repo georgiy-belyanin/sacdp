2ab60259e7857f0ad86a773d52ef055b26daceaf
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-04-17 13:59:44 +0300

    task-4: change dataset to more suitable

--- task-4/download.sh
+++ task-4/download.sh
@@ -1,11 +1,6 @@
 mkdir -p data
 cd data
-wget https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/RND3SAT/uf20-91.tar.gz
-wget https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/RND3SAT/uf50-218.tar.gz
-wget https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/RND3SAT/uuf50-218.tar.gz
-wget https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/RND3SAT/uf100-430.tar.gz
-wget https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/RND3SAT/uuf100-430.tar.gz
-mv "UUF50.218.1000"/* .
+wget https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/DIMACS/JNH/jnh.tar.gz
 for f in *.tar.gz; do tar -xzvf "$f"; done
 rm -r *.tar.gz
 cd ..
