69290b6a13adbc9842a7c602e2ee235b4b25c024
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-05-22 18:34:24 +0300

    task-3: add aes decoder

--- task-3/README.md
+++ task-3/README.md
@@ -1,7 +1,7 @@
 AES-like character device
 =========================
 
-A simple Linux kernel module representing character device, simulating one of the AES stages
+A simple Linux 6.0+ kernel module representing character device, simulating one of the AES stages
 
 Building:
 --------
@@ -18,10 +18,9 @@ sudo insmod aes.ko
 Example usage:
 --------------
 ```bash
-sudo dd if=source if=/dev/aes-0 bs=16 # writing, first 256 bytes are key, later - are values to encode
-sudo dd if=/dev/aes-0 of=dest bs=16 count=5 #reading
+sudo dd if=source if=/dev/aes0 bs=16 # encoding, first 256 bytes are key, later - are values to encode
+sudo dd if=/dev/aes0 of=dest bs=16 #reading encoded string
+sudo dd if=dest if=/dev/aes1 bs=16 #decoding
+sudo dd if=/dev/aes1 of=dest bs=16 #reading decoding results
 ```
 
-Known problems:
----------------
-On some systems `class_create` is defined without first argument, these can be fixed using `aes_main-fix-class_create.patch`
--- task-3/aes_main.c
+++ task-3/aes_main.c
@@ -19,6 +19,11 @@ MODULE_AUTHOR("Georgiy Belyanin");
 MODULE_DESCRIPTION("AES-like character dev");
 MODULE_VERSION("0.1");
 
+enum mode {
+    ENCODE,
+    DECODE
+};
+
 struct cdev_data {
     struct cdev cdev;
 };
@@ -27,14 +32,18 @@ struct aes_block {
     struct aes_block *next;
 };
 
-static int cdev_major = 0;
 static struct class *aesdev_class = NULL;
-static struct cdev_data aesdev_data;
-static unsigned char key[AES_KEY_SIZE];
-static unsigned int key_index;
+static int aes_major = 0;
+static struct cdev_data encoder_dev_data;
+static struct cdev_data decoder_dev_data;
 static struct aes_block *head;
 static struct aes_block *tail;
+
+static unsigned char key[AES_KEY_SIZE];
+static unsigned int key_index;
+
 static unsigned char shuffle[AES_BLOCK_SIZE] = { 5, 7, 4, 6, 13, 15, 12, 14, 1, 3, 0, 2, 9, 11, 8, 10 };
+static unsigned char deshuffle[AES_BLOCK_SIZE] = { 10, 8, 11, 9, 2, 0, 3, 1, 14, 12, 15, 13, 6, 4, 7, 5 };
 
 static struct aes_block *push_block(void) {
     struct aes_block *aes_block = kmalloc(sizeof(*aes_block), GFP_KERNEL);
@@ -61,17 +70,18 @@ static struct aes_block *pop_block(void) {
     return aes_block;
 }
 
-static int transform_block(struct aes_block *aes_block) {
+static int encode_block(struct aes_block *aes_block) {
     size_t i, j;
     unsigned char h;
     unsigned char result[AES_BLOCK_SIZE];
+    f_t f, g;
 
     for (j = 0; j < AES_KEY_SIZE / AES_BLOCK_SIZE; j++) {
         for (i = 0; i < AES_BLOCK_SIZE; i++) {
-            f_t f = f_from_uint8(aes_block->values[i]);
+            f = f_from_uint8(aes_block->values[i]);
             if (f == NULL) return -ENOMEM;
 
-            f_t g = f_inv(f);
+            g = f_inv(f);
             destroy_f(f);
             if (g == NULL) return -ENOMEM;
             h = f_to_uint8(g);
@@ -82,6 +92,35 @@ static int transform_block(struct aes_block *aes_block) {
 
         for (i = 0; i < AES_BLOCK_SIZE; i++)
             result[i] = aes_block->values[shuffle[i]];
+        for (i = 0; i < AES_BLOCK_SIZE; i++)
+            aes_block->values[i] = result[i];
+    }
+    return 0;
+}
+static int decode_block(struct aes_block *aes_block) {
+    size_t i;
+    int32_t j;
+    unsigned char result[AES_BLOCK_SIZE];
+    f_t f, g;
+
+    for (j = AES_KEY_SIZE / AES_BLOCK_SIZE - 1; j >= 0; j--) {
+        for (i = 0; i < AES_BLOCK_SIZE; i++)
+            result[i] = aes_block->values[deshuffle[i]];
+
+        for (i = 0; i < AES_BLOCK_SIZE; i++) {
+            result[i] ^= key[i + j * AES_BLOCK_SIZE];
+
+            f = f_from_uint8(result[i]);
+            if (f == NULL) return -ENOMEM;
+
+            g = f_inv(f);
+            destroy_f(f);
+            if (g == NULL) return -ENOMEM;
+
+            result[i] = f_to_uint8(g);
+            destroy_f(g);
+        }
+
         for (i = 0; i < AES_BLOCK_SIZE; i++)
             aes_block->values[i] = result[i];
     }
@@ -108,7 +147,7 @@ static ssize_t cdev_read(struct file *file, char __user *buf, size_t count, loff
     }
 
     for (i = 0; i < block_count && (aes_block = pop_block()) != NULL; i++) {
-        err = copy_to_user(buf, aes_block->values, AES_BLOCK_SIZE);
+        err = copy_to_user(buf + i * AES_BLOCK_SIZE, aes_block->values, AES_BLOCK_SIZE);
         if (err != 0) {
             printk(KERN_ERR "Copy data to user failed\n");
             return -EFAULT;
@@ -116,17 +155,15 @@ static ssize_t cdev_read(struct file *file, char __user *buf, size_t count, loff
         kfree(aes_block);
     }
 
-    return count;
+    return i * AES_BLOCK_SIZE;
 }
 
-static ssize_t cdev_write(struct file *file, const char __user *buf, size_t count, loff_t *offset) {
+static ssize_t cdev_write(enum mode mode, struct file *file, const char __user *buf, size_t count, loff_t *offset) {
     size_t i;
     size_t n;
     size_t key_read = 0;
     struct aes_block *aes_block;
     int err;
-
-    printk(KERN_DEBUG "Entering: %s\n", __func__);
 
     if (count % AES_BLOCK_SIZE != 0) {
         printk(KERN_ERR "Expected write size as a multiple of AES_BLOCK_SIZE: %lu\n", count);
@@ -158,8 +195,13 @@ static ssize_t cdev_write(struct file *file, const char __user *buf, size_t coun
             printk(KERN_ERR "Copy data from user failed\n");
             return -EFAULT;
         }
-        err = transform_block(aes_block);
-        if (n < 0) {
+
+        if (mode == ENCODE)
+            err = encode_block(aes_block);
+        else
+            err = decode_block(aes_block);
+
+        if (err < 0) {
             printk(KERN_ERR "Error while transforming aes block, probably no memory left\n");
             return -EFAULT;
         }
@@ -168,10 +210,28 @@ static ssize_t cdev_write(struct file *file, const char __user *buf, size_t coun
     return count;
 }
 
-static const struct file_operations cdev_fops = {
-    .owner    = THIS_MODULE,
-    .read    = cdev_read,
-    .write   = cdev_write
+static ssize_t encoder_write(struct file *file, const char __user *buf, size_t count, loff_t *offset) {
+    return cdev_write(ENCODE, file, buf, count, offset);
+}
+static ssize_t encoder_read(struct file *file, char __user *buf, size_t count, loff_t *offset) {
+    return cdev_read(file, buf, count, offset);
+}
+static ssize_t decoder_write(struct file *file, const char __user *buf, size_t count, loff_t *offset) {
+    return cdev_write(DECODE, file, buf, count, offset);
+}
+static ssize_t decoder_read(struct file *file, char __user *buf, size_t count, loff_t *offset) {
+    return cdev_read(file, buf, count, offset);
+}
+
+static const struct file_operations encoder_fops = {
+    .owner   = THIS_MODULE,
+    .read    = encoder_read,
+    .write   = encoder_write
+};
+static const struct file_operations decoder_fops = {
+    .owner   = THIS_MODULE,
+    .read    = decoder_read,
+    .write   = decoder_write
 };
 
 int init_module(void) {
@@ -182,31 +242,45 @@ int init_module(void) {
 
     printk(KERN_DEBUG "Entering: %s\n", __func__);
 
-    err = alloc_chrdev_region(&dev, 0, 1, "aes");
+    err = alloc_chrdev_region(&dev, 0, 2, "aes");
     if (err < 0) {
-        printk(KERN_ERR "Allocate a range of char device numbers failed.\n");
+        printk(KERN_ERR "Allocate a range of char device numbers for AES failed.\n");
         result = err;
         goto cleanup_0;
     }
+    aes_major = MAJOR(dev);
+    printk(KERN_DEBUG "AES device major number is: %d\n", aes_major);
 
-    cdev_major = MAJOR(dev);
-    printk(KERN_DEBUG "AES device major number is: %d\n", cdev_major);
-    if (IS_ERR(aesdev_class = class_create("aes"))) {
+    if (IS_ERR(aesdev_class = class_create(THIS_MODULE, "aes"))) {
         result = PTR_ERR(aesdev_class);
         goto cleanup_1;
     }
 
-    if (IS_ERR(dev_ret = device_create(aesdev_class, NULL, MKDEV(cdev_major, 0), NULL, "aes-0"))) {
+    if (IS_ERR(dev_ret = device_create(aesdev_class, NULL, MKDEV(aes_major, 0), NULL, "aes0"))) {
         result = PTR_ERR(dev_ret);
         goto cleanup_3;
     }
+    cdev_init(&encoder_dev_data.cdev, &encoder_fops);
+    encoder_dev_data.cdev.owner = THIS_MODULE;
 
-    cdev_init(&aesdev_data.cdev, &cdev_fops);
-    aesdev_data.cdev.owner = THIS_MODULE;
-
-    err = cdev_add(&aesdev_data.cdev, MKDEV(cdev_major, 0), 1);
+    err = cdev_add(&encoder_dev_data.cdev, MKDEV(aes_major, 0), 1);
     if (err < 0) {
-        printk (KERN_ERR "Unable to add AES char device\n");
+        printk (KERN_ERR "Unable to add AES encoder char device\n");
+        result = err;
+        goto cleanup_4;
+    }
+
+    if (IS_ERR(dev_ret = device_create(aesdev_class, NULL, MKDEV(aes_major, 1), NULL, "aes1"))) {
+        result = PTR_ERR(dev_ret);
+        goto cleanup_4;
+    }
+
+    cdev_init(&decoder_dev_data.cdev, &decoder_fops);
+    decoder_dev_data.cdev.owner = THIS_MODULE;
+
+    err = cdev_add(&decoder_dev_data.cdev, MKDEV(aes_major, 1), 1);
+    if (err < 0) {
+        printk (KERN_ERR "Unable to add AES decoder char device\n");
         result = err;
         goto cleanup_4;
     }
@@ -218,12 +292,14 @@ int init_module(void) {
     goto cleanup_0;
 
 cleanup_4:
-    device_destroy(aesdev_class, MKDEV(cdev_major, 0));
+    device_destroy(aesdev_class, MKDEV(aes_major, 0));
+    device_destroy(aesdev_class, MKDEV(aes_major, 1));
 cleanup_3:
     class_unregister(aesdev_class);
     class_destroy(aesdev_class);
 cleanup_1:
-    unregister_chrdev_region(MKDEV(cdev_major, 0), 1);
+    unregister_chrdev_region(MKDEV(aes_major, 0), 1);
+    unregister_chrdev_region(MKDEV(aes_major, 1), 1);
 cleanup_0:
     return result;
 }
@@ -231,9 +307,11 @@ cleanup_0:
 void cleanup_module(void) {
     printk(KERN_DEBUG "Entering: %s\n", __func__);
     block_cleanup();
-    device_destroy(aesdev_class, MKDEV(cdev_major, 0));
+    device_destroy(aesdev_class, MKDEV(aes_major, 0));
+    device_destroy(aesdev_class, MKDEV(aes_major, 1));
     class_unregister(aesdev_class);
     class_destroy(aesdev_class);
-    unregister_chrdev_region(MKDEV(cdev_major, 0), 1);
+    unregister_chrdev_region(MKDEV(aes_major, 0), 1);
+    unregister_chrdev_region(MKDEV(aes_major, 1), 1);
 }
 
